87750f715cc9a1b2d9c65adb9a82cb8ba91e190e
Nate Jensvold <jensvoldnate@gmail.com>
2021-04-18 16:42:07 -0700

    Added setup.py and renamed readme

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from setuptools import setup
 
-with open("README", 'r') as f:
+with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(
-    name='foo',
+    name='afk-processing',
     version='1.0',
-    description='A useful module',
+    description='A module for processing afk arena screenshots into '
+    'textual information',
     license="MIT",
     long_description=long_description,
-    author='Man Foo',
-    author_email='foomail@foo.com',
-    url="http://www.foopackage.com/",
-    packages=['foo'],  # same as name
+    author='Nate Jensvold',
+    author_email='jensvoldnate@gmail.com',
+    # url="http://www.foopackage.com/",
+    packages=['image_processing'],  # same as name
     # external packages as dependencies
-    install_requires=['wheel', 'bar', 'greek'],
+    install_requires=['wheel', 'bar'],
     scripts=[
-        'scripts/cool',
-        'scripts/skype',
+
     ]
 )
