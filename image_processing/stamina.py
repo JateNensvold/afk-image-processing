ac7c9aa5eb1567b587684ca6de4409416304423a
Nate Jensvold <jensvoldnate@gmail.com>
2021-05-19 16:01:31 -0700

    Stamina extraction on text image now text and workiong, cleaned up stamina.py, info is dumpted to terminal

--- image_processing/stamina.py
+++ image_processing/stamina.py
@@ -5,10 +5,7 @@ import matplotlib.pyplot as plt
 import image_processing.load_images as load
 
 import image_processing.processing as processing
-import pytesseract
 import imutils
-
-# pytesseract.pytesseract.tesseract_cmd = 'System_path_to_tesseract.exe'
 
 
 def sort_row(row: list, heroes: dict):
@@ -39,7 +36,6 @@ def generate_rows(heroes: dict, spacing: int = 10):
     for i in range(len(rows)):
         newRow = sort_row(rows[i], heroes)
         rows[i] = newRow
-    print(rows)
 
     return rows
 
@@ -61,10 +57,8 @@ def get_stamina_area(rows: list, heroes: dict, sourceImage: np.array):
             gapStartY = y[1]
             gapWidth = x[1] - x[0]
             if (j + 1) == numRows:
-                # gapBottom = sourceImage.shape[1]
                 gapBottom = gapStartY + int(averageHeight)
             else:
-                print(heroes.keys())
                 gapBottom = heroes[rows[j+1][i][1]]["dimensions"]["y"][0]
 
                 samples += 1
@@ -76,28 +70,28 @@ def get_stamina_area(rows: list, heroes: dict, sourceImage: np.array):
                                       gapStartX:gapStartX + gapWidth]
             staminaCount[unitName] = staminaArea
 
-            # cv2.rectangle(sourceImage, (gapStartX, gapStartY),
-            #               (gapStartX + gapWidth, gapBottom),
-            #               (0, 0, 255), 3)
-
-            # print(staminaArea.shape)
-            # plt.figure()
-            # plt.imshow(sourceImage)
-            # plt.show()
-
-            # plt.figure()
-            # plt.imshow(staminaArea)
-            # plt.show()
-
-        # print("({},{})x({},{})".format(x_30, x-x_30, y_30, y-y_30))
-        # crop_img = output[y_30: y-y_30, x_30: x-x_30]
     return staminaCount
 
 
-def get_text(staminaAreas: dict):
+def get_text(staminaAreas: dict, train: bool = False):
+
+    # build template dictionary
+    digits = {}
+    numbersFolder = os.path.join(os.path.dirname(
+        os.path.abspath(__file__)), "numbers")
+
+    referenceFolders = os.listdir(numbersFolder)
+    for folder in referenceFolders:
+        if folder not in digits:
+            digits[folder] = {}
+        digitFolder = os.path.join(numbersFolder, folder)
+        for i in os.listdir(digitFolder):
+            name, ext = os.path.splitext(i)
+            digits[folder][name] = cv2.imread(os.path.join(digitFolder, i))
+    output = {}
     for name, stamina_image in staminaAreas.items():
         original = stamina_image.copy()
-# (hMin = 0 , sMin = 0, vMin = 176), (hMax = 174 , sMax = 34, vMax = 255)
+
         lower = np.array([0, 0, 176])
         upper = np.array([174, 34, 255])
         hsv = cv2.cvtColor(stamina_image, cv2.COLOR_BGR2HSV)
@@ -109,34 +103,55 @@ def get_text(staminaAreas: dict):
         result = cv2.threshold(
             result, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)[1]
 
-        # text = pytesseract.image_to_string(
-        #     result, lang='eng', config='--psm 11')
-        # print("name: {} stamina: {}".format(name, text))
-        # loop over the OCR-A reference contours
-
-        contours = cv2.findContours(
+        digitCnts = cv2.findContours(
             mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        contours = contours[0] if len(contours) == 2 else contours[1]
-        for cntr in contours:
-            x, y, w, h = cv2.boundingRect(cntr)
-            # cv2.rectangle(stamina_image, (x, y), (x+w, y+h), (0, 0, 255), 2)
-            print("x,y,w,h:", x, y, w, h)
-            ROI = stamina_image[y:y+h, x:x+w]
+        digitCnts = imutils.grab_contours(digitCnts)
+        digitCnts = imutils.contours.sort_contours(digitCnts,
+                                                   method="left-to-right")[0]
+        digitText = []
+        for digit in digitCnts:
+            x, y, w, h = cv2.boundingRect(digit)
+            if w > 6 and h > 12:
+                ROI = stamina_image[y:y+h, x:x+w]
+                sizedROI = cv2.resize(ROI, (57, 88))
+                if train:
+                    digitFeatures(sizedROI)
+                else:
 
-            digitFeatures(ROI)
+                    numberScore = []
 
-    # plt.imshow(mask)
-    # plt.show()
-    # plt.imshow(result)
-    # plt.show()
+                    for digitName, digitDICT in digits.items():
+                        scores = []
+                        for digitIteration, digitImage in digitDICT.items():
+                            templateMatch = cv2.matchTemplate(
+                                sizedROI, digitImage, cv2.TM_CCOEFF)
+                            (_, score, _, _) = cv2.minMaxLoc(templateMatch)
+                            scores.append(score)
+                        avgScore = sum(scores)/len(scores)
+                        numberScore.append((digitName, avgScore))
+                    temp = sorted(
+                        numberScore, key=lambda x: x[1], reverse=True)
+                    digitText.append(temp[0][0])
+
+        text = "".join(digitText)
+        output[name] = text
+    return output
 
 
 def digitFeatures(digit: np.array):
+    """
+    Save a presized digit to whatever number is entered
+    Args:
+        digit: presized image of a digit, that will be saved as a training
+            template under whatever digitName/label is entered when prompted
+            by terminal
+    Return:
+        None
+    """
     baseDir = "./numbers"
     os.listdir(baseDir)
     plt.figure()
     plt.imshow(digit)
-    roi = cv2.resize(digit, (57, 88))
     print("Please enter the number shown in the image after you close it: ")
     plt.show()
     number = input()
@@ -144,7 +159,7 @@ def digitFeatures(digit: np.array):
     numberLen = str(len(os.listdir(numberDir)))
     numberName = os.path.join(numberDir, numberLen)
 
-    cv2.imwrite("{}.png".format(numberName), roi)
+    cv2.imwrite("{}.png".format(numberName), digit)
 
 
 if __name__ == "__main__":
@@ -154,25 +169,33 @@ if __name__ == "__main__":
     baseImages = []
     for i in files:
         hero = cv2.imread(i)
-        name = os.path.basename(i)
+        baseName = os.path.basename(i)
+        name, ext = os.path.splitext(baseName)
+
         baseImages.append((name, hero))
 
     # load in screenshot of heroes
     stamina_image = cv2.imread("./stamina-2.jpg")
-    # plt.imshow(stamina_image)
-    # plt.show()
     heroesDict = processing.getHeroes(stamina_image)
-    print(len(heroesDict))
 
     cropHeroes = load.crop_heroes(heroesDict)
-    print(len(cropHeroes))
     imageDB = load.build_flann(baseImages)
 
     for k, v in cropHeroes.items():
 
-        name, baseHeroImage = imageDB.search(v, display=True)
+        name, baseHeroImage = imageDB.search(v, display=False)
         heroesDict[k]["label"] = name
 
     rows = generate_rows(heroesDict)
     staminaAreas = get_stamina_area(rows, heroesDict, stamina_image)
-    get_text(staminaAreas)
+    staminaOutput = get_text(staminaAreas)
+    output = {}
+
+    for name, text in staminaOutput.items():
+        label = heroesDict[name]["label"]
+        if label not in output:
+            output[label] = {}
+        output[label]["stamina"] = text
+    import json
+    outputJson = json.dumps(output)
+    print(outputJson)
